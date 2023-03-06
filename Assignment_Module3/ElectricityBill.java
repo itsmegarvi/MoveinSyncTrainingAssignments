b88eb4a8cd3e84756888606f6d9f4d44abde7ea3
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-06 19:25:12 +0530

    Assignment2 of module3 for moveinsync training

---
+++ Assignment_Module3/ElectricityBill.java
@@ -0,0 +1,23 @@
+package Assignment_Module3;
+
+import java.util.Scanner;
+
+public class ElectricityBill {
+    public static void main(String[] args) {
+        int prev_read, new_read;
+        Scanner sc = new Scanner(System.in);
+        prev_read = sc.nextInt();
+        new_read = sc.nextInt();
+        float bill = 0.0f;
+        int units_consumed = new_read - prev_read;
+        if(units_consumed<=30){
+            bill += units_consumed*2.3f;
+        } else if (units_consumed<=70){
+            bill += (units_consumed-30)*3.5f + (30*2.3f);
+        } else{
+            bill = (units_consumed-70)*4.6f + (30*2.3f) + (40*3.5f);
+        }
+
+        System.out.printf("Electricity bill is: Rs" + "%.2f",bill);
+    }
+}
