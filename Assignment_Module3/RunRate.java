3b59b5dad706ebebf59735924dcddaaa784797b2
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-06 17:21:02 +0530

    Assignment1 of module3 for moveinsync training

---
+++ Assignment_Module3/RunRate.java
@@ -0,0 +1,21 @@
+package Assignment_Module3;
+
+import java.util.Scanner;
+
+public class RunRate {
+    public static void main(String[] args) {
+        int overs,target;
+        float cr;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of overs bowled: ");
+        overs = sc.nextInt();
+        System.out.println("Enter the current run-rate: ");
+        cr = sc.nextFloat();
+        System.out.println("Enter the target for run-chase: ");
+        target = sc.nextInt();
+
+        float req_run_rate = (target-(cr*overs))/(50-overs);
+        System.out.println("Required run-rate is: " + req_run_rate);
+    }
+}
