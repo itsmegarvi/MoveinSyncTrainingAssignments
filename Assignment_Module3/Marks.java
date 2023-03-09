bad9edace7e0fb8d386b4545e338d4e55242baef
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-09 19:32:36 +0530

    Assignment10 of module3 for moveinsync training

---
+++ Assignment_Module3/Marks.java
@@ -0,0 +1,18 @@
+package Assignment_Module3;
+
+import java.util.*;
+
+public class Marks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> l = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            l.add(sc.nextInt());
+        }
+
+        System.out.println(l.stream().max(Integer::compare).orElseThrow(NoSuchElementException::new));
+        System.out.println(l.stream().min(Integer::compare).orElseThrow(NoSuchElementException::new));
+        System.out.println(l.stream().mapToDouble(x->x).average().orElseThrow(NoSuchElementException::new));
+    }
+}
