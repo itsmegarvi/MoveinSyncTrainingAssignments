f658013373606d2af92a33a508ba8d78414f0d01
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-09 23:24:01 +0530

    Assignment13 of module3 for moveinsync training //module 3 ends

---
+++ Assignment_Module3/PTDrill.java
@@ -0,0 +1,32 @@
+package Assignment_Module3;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class PTDrill {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<String> l = new ArrayList<>();
+        for(int i =0;i<n;i++){
+            l.add(sc.next());
+        }
+        double h = Math.sqrt(n);
+        int j = (int) h;
+        final List<String> o = l.stream().sorted((a,b)->a.compareTo(b)).collect(Collectors.toList());
+        List<List<String>> ls = IntStream.range(0,l.size()/j).mapToObj(a->o.subList((a*j),(a+1)*j)).collect(Collectors.toList());
+
+
+        List<String> middleRow = ls.get(ls.size() / 2);
+        System.out.println("Middle row: " + middleRow);
+
+        List<String> middleColumn = ls.stream()
+                .map(row -> row.get(row.size() / 2))
+                .collect(Collectors.toList());
+        System.out.println("Middle column: " + middleColumn);
+    }
+}
