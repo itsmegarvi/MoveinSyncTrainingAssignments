a6e6e5423b7040b23e6c176b3dac192cf9c1039c
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-10 18:46:04 +0530

    Ques 3 of Module4

---
+++ com/Module4/Constructor.java
@@ -0,0 +1,26 @@
+package com.Module4;
+
+class Product1{
+    private String productName;
+    private String productCode;
+    private double productPrice;
+
+    Product1(String pn,String pc, double pp){
+        productCode = pc;
+        productName = pn;
+        productPrice = pp;
+    }
+
+    void disp(){
+        System.out.println("Product Code :" + productCode);
+        System.out.println("Product Name :" + productName);
+        System.out.println("Product Price :" + productPrice);
+    }
+}
+public class Constructor {
+    public static void main(String[] args) {
+        Product1 p = new Product1("p105","Brush",45.25);
+        p.disp();
+
+    }
+}
