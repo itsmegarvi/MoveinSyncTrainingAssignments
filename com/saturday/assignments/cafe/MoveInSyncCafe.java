35531523ef1755e43cc425d3c8f439387bb8b338
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-11 16:21:09 +0530

    Saturday Assignment for march week 2 waiter and main class preliminary work

--- com/saturday/assignments/cafe/MoveInSyncCafe.java
+++ com/saturday/assignments/cafe/MoveInSyncCafe.java
@@ -1,5 +1,16 @@
 package com.saturday.assignments.cafe;
 
+import java.util.Scanner;
+
 public class MoveInSyncCafe {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("No.of chefs");
+        int chefs = sc.nextInt();
+//        Waiter w = new Waiter()
+        //just thought of something
+        //If we are making a constructor for waiter and making it do the job of reaching to the chef it will take a lot of objects
+        //And that is inefficient, so this should be a function 1 waiter unless specified presence of more
+
+    }
 }
--- com/saturday/assignments/cafe/Waiter.java
+++ com/saturday/assignments/cafe/Waiter.java
@@ -1,4 +1,27 @@
 package com.saturday.assignments.cafe;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.saturday.assignments.cafe.Menu.Foods;
+
 public class Waiter {
+    List<List<Foods>> order = new ArrayList<>();
+    public Waiter(List<Foods> f){
+        order.add(f);
+    } //This approach might not work considering the order might keep coming later on.
+    // We cant be sending the same list with just addition of one order everytime to chef.
+
+    //What can be the alternatives:
+    //Instead of sending the list to chef,
+    // We can make a queue in chef that when not empty starts cooking
+            // But for this we might also want a function that on addition to queue returns order no. + order place time
+    //If we do this with priority queue then there would be a function doing totally different jobs
+
+    //In conclusion, it seems till we are not clear about how to solve the main problem, the rest will be subject to change and a
+    //waste of time in my opinion
+
+    //Also am in love with big monitor coding
+    // I want it I want it I want it
+
+
 }
