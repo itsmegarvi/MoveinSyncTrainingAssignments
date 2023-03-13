85e07d4d07c152b422a11b83c6bfd6ff3e12f128
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-13 15:55:43 +0530

    Saturday Assignment for march week 2 Chef.java ready for beta version

--- com/saturday/assignments/cafe/Chef.java
+++ com/saturday/assignments/cafe/Chef.java
@@ -1,11 +1,54 @@
 package com.saturday.assignments.cafe;
 
-import java.util.Queue;
+import com.saturday.assignments.Order;
+
+import java.time.LocalTime;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class Chef {
     private int numChefs;
+    private int lastCompletedTime;
+    public Chef(int n){
+        numChefs = n;
+        lastCompletedTime = 0;
+    }
 
-    public void processorder(int ordernum){
+    public int getLastCompletedTime() {
+        return lastCompletedTime;
+    }
+
+    public void processorder(int ordernum, int prevEndTime){
+        Order order = new Order(ordernum);
+        if(Order.orders.isEmpty()) {
+            List<Integer> l = order.getOrders(ordernum-1);
+            PriorityQueue<Integer> pq = new PriorityQueue<>(numChefs);
+            pq.clear();
+
+            l.sort(Integer::compare);
+            for(int k=l.size() -1;k>=0;k--){
+                if(pq.size() < numChefs){
+                    pq.add(l.get(k));
+                }else{
+                    int minTime = pq.poll();
+                    pq.add(minTime + l.get(k));
+                }
+            }
+            while (pq.size() != 1){
+                pq.poll();
+            }
+
+            int servetime = pq.peek() + prevEndTime;
+
+            lastCompletedTime = servetime;
+
+
+            System.out.println("You will get your order at " + LocalTime.now().plusMinutes(servetime));
+
+        }
+        else{
+            System.out.println("NOt getting added to List");
+        }
 
     }
 
